de04bcf28ae269bde53de489b0b052ff00d661c9
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-09-24 20:46:03 -0300

    Change system name

--- app/apontamento/views.py
+++ app/apontamento/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
-def apontamento_list(request):
+@login_required
+def apontamento_list(request, login_url="users:login"):
     return render(request, "apontamento/apontamento-list.html", {})
