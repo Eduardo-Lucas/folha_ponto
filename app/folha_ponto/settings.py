debf7a676e5c5823a6bee006b321fd4be6266fd1
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-09-24 20:23:23 -0300

    Login and Logout working on

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "users:login"
